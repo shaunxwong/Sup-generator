b777c93428c3cf3eeda7964bf116e69316ca6791
shaunxwong <shaunxwong@gmail.com>
2023-06-14 21:51:11 +1000

    fix comments in JS

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Get references to the necessary DOM elements
+// Get references to the necessary DOM elements.
 let generateBtn = document.querySelector("#generate");
 let copyBtn = document.querySelector("#copy");
 let passwordText = document.querySelector("#password");
 let lengthInput = document.querySelector("#length");
 let lengthValue = document.querySelector("#lengthValue");
 
-// Function to write the generated password to the password textarea
+// Function to write the generated password to the password textarea.
 function writePassword() {
   let password = generatePassword();
   passwordText.value = password;
@@ -13,12 +13,12 @@ function writePassword() {
 function generatePassword() {
   let password = "";
 
-  // Get the password length from the length input element
+  // Get the password length from the length input element.
   let length = parseInt(lengthInput.value);
 
   // Get the checkbox elements for character types
   let lowercaseCheckbox = document.querySelector("#lowercase");
-  let uppercaseCheckbox = document.querySelector("#uppercase");
+  let uppercaseCheckbox = document.querySelectorwod("#uppercase");
   let numericCheckbox = document.querySelector("#numeric");
   let specialCheckbox = document.querySelector("#special");
 
@@ -45,7 +45,8 @@ function generatePassword() {
   let numericChars = "0123456789";
   let specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
-  // 
+  // Created an empty string called availableChars by fusing different character pools based on what checkboxes people have selected. It essentially gathers all the chosen characters for generating the password into one string.
+
   let availableChars = "";
   if (lowercaseCheckbox.checked) {
     availableChars += lowercaseChars;
@@ -60,24 +61,24 @@ function generatePassword() {
     availableChars += specialChars;
   }
 
-  // Generate the password based on user criteria. 
+  // Generate the password using a for loop that iterates length number of times. Each iteration is a random index using the 'math.random' function and selecting a character from the 'availableChar' string. Chosen character is then added to the password. 
   for (let i = 0; i < length; i++) {
     let randomIndex = Math.floor(Math.random() * availableChars.length);
     password += availableChars.charAt(randomIndex);
   }
 
-  // Return the generated password
+  // Return the generated password.
   return password;
 }
 
-// Function to update the displayed value of the password length slider
+// Function updates the displayed value of the password length slider.
 function updateLengthValue() {
   lengthValue.textContent = lengthInput.value;
 }
 
-// Add event listeners for button click and slider input
+// Add event listeners for button click and slider input.
 generateBtn.addEventListener("click", writePassword);
 lengthInput.addEventListener("input", updateLengthValue);
 
-// Update the displayed value of the password length initially
+// Update the displayed value of the password length initially.
 updateLengthValue();
